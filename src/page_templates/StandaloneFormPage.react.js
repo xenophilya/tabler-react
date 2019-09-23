753abddc5158f5d1d2924a495df2e880ead598a3
xenophilya <manuelemeka@gmail.com>
2019-09-23 15:56:12 +0100

    Removed StandAloneFormPage logo

--- src/page_templates/StandaloneFormPage.react.js
+++ src/page_templates/StandaloneFormPage.react.js
@@ -4,7 +4,7 @@ import * as React from "react";
 
 type Props = {|
   +children?: React.Node,
-  +imageURL?: string,
+  
 |};
 
 function StandaloneFormPage(props: Props): React.Node {
@@ -15,7 +15,7 @@ function StandaloneFormPage(props: Props): React.Node {
           <div className="row">
             <div className="col col-login mx-auto">
               <div className="text-center mb-6">
-                <img src={props.imageURL} className="h-6" alt="logo" />
+                
               </div>
               {props.children}
             </div>
